7f75dffa73345c7889839c97bbee5aac5255cf21
Morridin <paulza@gmx.de>
2022-12-05 10:18:24 +0100

    Wrote down the wanted program flow of bulk mailer on console.
    It's within the file doc string.

--- bulk-mailer.py
+++ bulk-mailer.py
@@ -3,6 +3,55 @@
 This file contains the routines that put the stuff in the lib package together to make a useful tool.
 
 To cut things short: Here goes bulk-mailer execution file.
+
+Wanted program flow:
+* User starts program without parameters.
+* Program shows a list of available actions and prompts for the wanted action
+  This list should include:
+    * Manage server connections
+        * View existing
+            * For each entry:
+                * Change settings
+                * Set as active (=currently used setup)
+                * Delete
+        * Add new
+            1. Enter SMTP server host name
+            2. Enter SMTP server port
+            3. Enter, if SMTP server needs encryption
+                * Enter, which type of encryption is necessary
+            4. Enter, if SMTP server needs login
+                * Choose between prompt on send or store persistent (unsafe)
+            5. Enter IMAP server host name
+            6. Enter IMAP server port
+            7. Enter, if IMAP server needs encryption
+                * Enter, which one.
+            8. Enter login data or say "use SMTP data"
+            9. Enter sender information ("From" field)
+                1. Enter sender name
+                2. Enter sender email address
+            10. Enter name for that server connection setup ("Save as")
+            11. Set as active?
+    * Manage recipients
+        * View list
+            * For each entry:
+                * Change
+                * Delete
+        * Add new
+            1. Enter recipient name
+            2. Enter recipient address
+            3. More e-mail addresses for this recipient?
+        * Load from file
+        * Store into file
+        * Clear ("Are you sure?")
+    * Set up email message (either by writing it directly in the program or by importing a file containing the message)
+        * Write here/modify
+        * Load from file
+        * Store into file
+        * Clear
+    * Get status (show current email message, recipients, selected server setup)
+    * Send email to recipients => show overview of action about to be started and let user confirm this.
+    * Reset application
+    * Exit
 """
 from argparse import ArgumentParser
 from smtplib import SMTP
@@ -21,7 +70,6 @@ def get_int(*, input_text: str = "Enter number ", error_text: str = "Input is no
     @return: The input int value, if any.
     """
     number_raw: str = input(input_text)
-    number: int = 0
     try:
         number = int(number_raw)
     except ValueError:
@@ -39,7 +87,6 @@ def get_int(*, input_text: str = "Enter number ", error_text: str = "Input is no
 
 if __name__ == "__main__":
     parser: ArgumentParser = commandline.Parser()
-
     args = parser.parse_args()
 
     if args.verboose:
