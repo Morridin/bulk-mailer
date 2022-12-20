de3a32aea37f88ac7092ffdbd238e028d1ea258a
Morridin <paulza@gmx.de>
2022-12-20 16:27:42 +0100

    Configured the lib package init file to match the new contents.

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,6 +4,19 @@ This package contains all the mechanics of the whole project.
 """
 __version__ = "0.0.1"
 
-__all__ = ["commandline", "__version__", "server_connections"]
+__all__ = ["commandline", "__version__", "server_connections", "recipients", "engine", "UI"]
+
+from abc import abstractmethod, ABCMeta
 
 
+class UI(metaclass=ABCMeta):
+    """
+    This is an abstract class defining some basic methods all valid user interfaces should have.
+    """
+
+    @abstractmethod
+    def init(self) -> None:
+        """
+        This method is used to set up a user interface upon application start.
+        """
+        pass
