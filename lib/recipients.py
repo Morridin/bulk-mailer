79196d2c01164997ff85f211294a2593180055c0
Morridin <paulza@gmx.de>
2022-12-20 18:26:03 +0100

    Implemented core functionality of Recipient class.
    Some representation/getting stuff is still open though.

--- lib/recipients.py
+++ lib/recipients.py
@@ -23,4 +23,28 @@ class Recipient(RecipientsListEntry):
     """
     This class models an email recipient contained in a RecipientsList.
     """
+    def __init__(self, name: str, *email: str):
+        self.name: str = name
+        self.addresses: list[str] = list(email)
+
+    def add_address(self, address: str) -> None:
+        """
+        This method adds an address to the recipient.
+        :param address: The address to be added.
+        :return: None
+        """
+        self.addresses.append(address)
+
+    def get_formatted(self) -> tuple[str]:
+        """
+        This method creates a tuple of strings that read like 'Recipient <address@example.com>'.
+        :return: See above.
+        """
+        out: list[str] = []
+        for address in self.addresses:
+            out.append(f"{self.name} <{address}>")
+        return tuple(out)
+
+
+class RecipientManagement:
     pass
