8627f5386afd39ad3ec59e37bb06700b0f045149
Morridin <paulza@gmx.de>
2023-01-04 16:32:28 +0100

    Corrected the version number spelling in lib/__init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,6 @@
 """
 This package contains all the mechanics of the whole project.
 """
-__version__ = "0.0.0"
+__version__ = "0.0.0 Α-1"
 
 __all__ = ["__version__", "server_connections", "recipients", "engine", "ui"]
