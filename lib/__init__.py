eec7f22075ccb03c1be4a866d19813a8c66bf1c1
Morridin <paulza@gmx.de>
2023-01-09 22:23:06 +0100

    Updated version number in lib/__init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,6 @@
 """
 This package contains all the mechanics of the whole project.
 """
-__version__ = "0.0.0 Α-1"
+__version__ = "0.0.1 Α-1"
 
 __all__ = ["__version__", "server_connections", "recipients", "engine", "ui"]
